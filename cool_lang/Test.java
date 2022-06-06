1d64c49aec949055584580008dab27ca7c0b950c
ross39 <Rheaney26@gmail.com>
2022-06-06 18:44:59 +0100

    Add files via upload

---
+++ cool_lang/Test.java
@@ -0,0 +1,9 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Test {
+    public static void main(String[] args) {
+        FileReader fr = new FileReader("jflex_test.java");
+    }
+}
---
+++ cool_lang/jflex_test.java
@@ -0,0 +1,6 @@
+public class jflex_test {
+    public static void main(String[] args) {
+        char c = '\n';
+        System.out.println("Hello world");
+    }
+}
